0d3d980ea7cda80d1bf80b4d0a77252ba08e877a
Ulrich Luebke <luebke@nufta.de>
2018-06-06 19:32:57 +0200

    Some work on worlcup version

--- src/de/nufta/kicktipper/KickTipper.java
+++ src/de/nufta/kicktipper/KickTipper.java
@@ -18,32 +18,93 @@ import de.nufta.kicktipper.Game.Result;
  */
 public class KickTipper {
 
-    private static final String[] SEASON_NAMES = { "blfr_2013_res.txt", "blfr_2014_res.txt", "blfr_2015_res.txt",
-            "blfr_2016_res.txt", "blfr_2017_res.txt" };
-    private static final int FIRST_YEAR = 2013;
-    private static final int PREDICT_YEAR = 2017;
+    enum Mode{LEAGUE, WORLD_CUP};
+    
+//    private static final String[] SEASON_NAMES = { "blfr_2013_res.txt", "blfr_2014_res.txt", "blfr_2015_res.txt",
+//            "blfr_2016_res.txt", "blfr_2017_res.txt" };
+//    private static final int FIRST_YEAR = 2013;
+//    private static final int PREDICT_YEAR = 2017;
 
-    private static final String PRED_GAMES = "blfr_next1.txt";
+//    private static final String PRED_GAMES = "blfr_pred.txt";
+    
+    private final Mode mode;
+    private final String tournamentName ;
+    private final String tournamentShortName ;
+    private final int startYear ;
+    private final int endYear ;
+    private final int predictionYear;
+    
+    /**
+     * Show debug printouts on console
+     */
+    private boolean debug = false;
+    
 
     List<Season> seasons = new ArrayList<Season>();
     RatingCalculator calc = new RatingCalculator();
-    Season predictions;
+    Season predictions;   
 
-    KickTipper() {
+    /**
+     * Creates a new Instance 
+     * 
+     * @param mode Tournament Mode (Necessary for fileformat parsing) // Data copy-pasted from flashscore.de
+     * @param tournamentName name of the tournament
+     * @param tournamentShortName short name for identifying files ("blfr" -> Bundesliga Frauen "wm" -> Fußballweltmeisterschaft)
+     * @param fromYear use historical data beginning with this year
+     * @param toYear use historical data up to and including this year
+     * @param predictionYear year of the tournament to predict
+     */
+    KickTipper(final Mode mode, 
+                    final String tournamentName, 
+                    final String tournamentShortName, 
+                    final int startYear, 
+                    final int endYear, 
+                    final int predictionYear) {
+        
+        
+        this.mode = mode;
+        this.tournamentName = tournamentName;
+        this.tournamentShortName = tournamentShortName;
+        this.startYear = startYear;
+        this.endYear = endYear;
+        this.predictionYear = predictionYear;        
+    }
+    
+    private void run() {
         loadSeasons();
         loadPredictions();
     }
-
-    void run() {
-
+    
+    /**
+     * Enable / disable debug output
+     */
+    void setDebug (boolean b) {
+        this.debug = b;
+    }
+    
+    boolean isDebug() {
+        return debug;
     }
 
     private void loadSeasons() {
-        int year = FIRST_YEAR;
+        int year = startYear;
         Season previousSeason = null;
-        for (String season : SEASON_NAMES) {
-            SeasonParser parser = new SeasonParser(year++, season, SeasonParser.SEASON_MODE, calc,
+        final String resultFilePrefix = tournamentShortName + "_";
+        final String resultFilePostfix = "_res.txt";        
+        while (year <= endYear) {
+            final String seasonFileName = resultFilePrefix + year + resultFilePostfix; 
+            SeasonParser parser = new SeasonParser(this, year, seasonFileName, SeasonParser.SEASON_MODE, calc,
                     previousSeason);
+            switch(mode) {
+                case LEAGUE: {
+                    year++; 
+                    break;
+                        }
+                case WORLD_CUP: {
+                    year += 4;
+                    break;
+                }
+            }
             Season s = parser.parse();
             s.rate();
             System.out.println("SeasonParser: " + s.toString() );
@@ -53,11 +114,19 @@ public class KickTipper {
     }
 
     private void loadPredictions() {
-        SeasonParser parser = new SeasonParser(PREDICT_YEAR,  PRED_GAMES, SeasonParser.PREDICT_MODE, calc, seasons.get(seasons.size()-1));
+        String predictionFileName = tournamentShortName + "_pred.txt";
+        SeasonParser parser = new SeasonParser(
+                    this,
+                    predictionYear,
+                    predictionFileName, 
+                    SeasonParser.PREDICT_MODE, 
+                    calc, 
+                    seasons.get(seasons.size()-1));
         predictions = parser.parse();
         ArrayList<Game>allGames = new ArrayList<Game>();
         for (Season season : seasons) {
-            if (season.year > FIRST_YEAR) { // not for first year
+            // In the first season the glicko rating is not yet accurate, so leave that out.
+            if (season.year > this.startYear) {
                 allGames.addAll(season.getGames());
             }
         }
@@ -136,12 +205,17 @@ public class KickTipper {
         System.out.println("Predictions: " + predictions.toString());
     }
 
+    public Mode getMode() {
+        return mode;
+    }
+
     /**
      * @param args
      */
     public static void main(String[] args) {
-        KickTipper tipper = new KickTipper();
+        KickTipper tipper = new KickTipper(Mode.LEAGUE, "Bundesliga Frauen", "blfr", 2013, 2017, 2017);
         tipper.run();
     }
+
 
 }
--- src/de/nufta/kicktipper/SeasonParser.java
+++ src/de/nufta/kicktipper/SeasonParser.java
@@ -9,8 +9,9 @@ import java.util.regex.Pattern;
 import org.goochjs.glicko2.RatingCalculator;
 
 /**
+ * Parse  a season's result data file (or a prediction file with the same format)
+ * 
  * @author ulrich.luebke
- *
  */
 public class SeasonParser {
     
@@ -20,29 +21,32 @@ public class SeasonParser {
     private enum Stage {INIT, GAME, HOME, AWAY, SCORE};
         
     private final RatingCalculator calc;
-    private Season previousSeason;
+    private final Season previousSeason;
     private Stage stage = Stage.INIT;
-    private int year;
-    private String fileName;
-    private Season season;
+    private final int year;
+    private final String fileName;
+    private final Season season;
     private int day = 0;
     private boolean predictMode;
     private Team homeTeam, awayTeam;
     private int homeScore, awayScore;
+    private KickTipper tipper;
     
-    private static Pattern spielTagPattern = Pattern.compile(".*SpielTag.*", Pattern.CASE_INSENSITIVE);
-    private static Pattern datePattern = Pattern.compile("\\d{1,2}+\\.\\d{1,2}+\\..*\\d{1,2}+:\\d{1,2}+.*", Pattern.CASE_INSENSITIVE);
-    private static Pattern resultPattern = Pattern.compile("\\d{1,2}+\\s*:\\s*\\d{1,2}+", Pattern.CASE_INSENSITIVE);
+    private static final Pattern SPIELTAG_PATTERN = Pattern.compile(".*SpielTag.*", Pattern.CASE_INSENSITIVE);
+    private static final Pattern DATE_PATTERN = Pattern.compile("\\d{1,2}+\\.\\d{1,2}+\\..*\\d{1,2}+:\\d{1,2}+.*", Pattern.CASE_INSENSITIVE);
+    private static final Pattern RESULT_PATTERN = Pattern.compile("\\d{1,2}+\\s*:\\s*\\d{1,2}+", Pattern.CASE_INSENSITIVE);
     
     /**
-     * 
+     * Create a new Instance
      */
-    public SeasonParser(int year, String fileName, boolean predictMode, RatingCalculator calc, Season previousSeason) {
+    SeasonParser(KickTipper tipper, int year, String fileName, boolean predictMode, RatingCalculator calc, Season previousSeason) {
+        this.tipper = tipper;
         this.year = year;
         this.fileName = fileName;
         this.predictMode = predictMode;
         this.calc = calc;
         this.previousSeason = previousSeason;
+        season = new Season(year, calc);
         init();
     }
     
@@ -52,8 +56,6 @@ public class SeasonParser {
     }
     
     public Season parse() {
-        season = new Season(year, calc);
-        
         try (BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("data/"+fileName)))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -70,11 +72,11 @@ public class SeasonParser {
     }
 
     private void processLine(String line) {
-        if (spielTagPattern.matcher(line).matches()) {
+        if (SPIELTAG_PATTERN.matcher(line).matches()) {
             newDay(line);
-        } else if (datePattern.matcher(line).matches()) {
+        } else if (DATE_PATTERN.matcher(line).matches()) {
             newGame(line);
-        } else if (resultPattern.matcher(line).matches()) {
+        } else if (RESULT_PATTERN.matcher(line).matches()) {
             result(line);
         } else if (line.isEmpty()) {
             noResult(line);
@@ -129,13 +131,13 @@ public class SeasonParser {
     }
   
     public static void main(String[] args) {
-        System.out.println(spielTagPattern.matcher("2342. Spieltag      nnn").matches());
-        System.out.println(datePattern.matcher("07.05. 11:00").matches());
-        System.out.println(datePattern.matcher("7.05. 1:00  mm").matches());
-        System.out.println(resultPattern.matcher("1 : 3").matches());
-        System.out.println(resultPattern.matcher("1: 3").matches());
-        System.out.println(resultPattern.matcher("1: 3").matches());
-        System.out.println(resultPattern.matcher("1:   30").matches());
+        System.out.println(SPIELTAG_PATTERN.matcher("2342. Spieltag      nnn").matches());
+        System.out.println(DATE_PATTERN.matcher("07.05. 11:00").matches());
+        System.out.println(DATE_PATTERN.matcher("7.05. 1:00  mm").matches());
+        System.out.println(RESULT_PATTERN.matcher("1 : 3").matches());
+        System.out.println(RESULT_PATTERN.matcher("1: 3").matches());
+        System.out.println(RESULT_PATTERN.matcher("1: 3").matches());
+        System.out.println(RESULT_PATTERN.matcher("1:   30").matches());
     }
     
 }
